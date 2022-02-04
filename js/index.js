653647291a18dcef20600c8c2842d915a49ee280
Agnese Spinella <agnesespinella@gmail.com>
2022-02-04 12:03:16 +0100

    final push

--- js/index.js
+++ js/index.js
@@ -2,14 +2,13 @@ const q = (selector) => document.querySelector(selector);
 
 
 const render = (container, items) => {
-  console.log(items);
 
   const elements = items.map((element) => 
     `<li>
       <h3>${element.name}</h3>
       <p><strong>Phone:</strong> <a href="tel:${element.phone}">${element.phone}</a></p>
-      <p><strong>Email:</strong> <a href="mailto:${element.email}">${element.email}</a></p>
-    </li>`
+      <p><strong>Email:</strong> <a href="mailto:${element.email}">${element.email}</a></p>  
+      </li>`
   );
   
   const content = elements.join('');
@@ -18,7 +17,6 @@ const render = (container, items) => {
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-  const form = q('form');
   const input = q('form input');
   const list = q('ul');
   const add = q('#add');
